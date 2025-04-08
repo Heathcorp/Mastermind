ea856050f36bfdb52807cc24cff0e3bec8437275
BenNippard <104749340+BenNippard@users.noreply.github.com>
2025-04-08 18:10:39 +1000

    Updated Migration Version to show Sample Programs

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ import { createStore } from "solid-js/store";
 const AppContext = createContext<AppContextProps>();
 
 // update this when you want the user to see new syntax
-const MIGRATION_VERSION = 3;
+const MIGRATION_VERSION = 4;
 
 const App: Component = () => {
   const [version, setVersion] = makePersisted(createSignal<number>(), {
